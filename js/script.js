8a2bf9a172d11ef99c9865494d730fd7c97a94a3
i1-kmv <i1-kmv@mail.ru>
2020-06-16 09:38:21 +0200

    Модифицировал модальное окно

--- js/script.js
+++ js/script.js
@@ -96,26 +96,36 @@ window.addEventListener("DOMContentLoaded", function () {
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-close]');
 
+
+     function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-          modal.classList.add('show');
-          modal.classList.remove('hide');
-          document.body.style.overflow = 'hidden';
-        });
+        btn.addEventListener('click', openModal);
     });
-    
+
+   
     function closeModal() {
         modal.classList.add('hide');
         modal.classList.remove('show');
         document.body.style.overflow = '';
     }
+
+
     modalCloseBtn.addEventListener('click', closeModal);
+
 
     modal.addEventListener('click', (e) => {
         if (e.target === modal) {
             closeModal();
         } 
     });
+
 
     document.addEventListener('keydown', (e) => {
         if (e.code === 'Escape' && modal.classList.contains('show')) {
@@ -124,7 +134,17 @@ window.addEventListener("DOMContentLoaded", function () {
     });
 
 
-   
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+
+    window.addEventListener('scroll', showModalByScroll);
 
 
 });
